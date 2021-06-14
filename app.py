27ada71739514bd2044d31015819afe3ad7c82ac
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-14 15:28:46 +0530

    added run apps btn

--- app.py
+++ app.py
@@ -13,4 +13,7 @@ frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
 openFile = tk.Button(root, text='Open File', padx=10, pady=5, fg='white', bg='#263D42')
 openFile.pack()
 
+runApps = tk.Button(root, text='Run Apps', padx=10, pady=5, fg='white', bg='#263D42')
+runApps.pack()
+
 root.mainloop()
