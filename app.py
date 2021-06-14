abbb610833dbf5c6a458a0cd437098657816c6d9
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-14 15:34:45 +0530

    get files

--- app.py
+++ app.py
@@ -3,6 +3,13 @@ from tkinter import filedialog, Text
 import os
 
 root = tk.Tk()
+apps = []
+
+def add_app():
+    filename = filedialog.askopenfilename(initialdir='/', title='Select file', filetypes=(("executables", "*.exe"), ("all files", "*.*")))
+    apps.append(filename)
+    print(filename)
+
 
 canvas = tk.Canvas(root, height=700, width=700, bg='#263D42')
 canvas.pack()
@@ -10,7 +17,7 @@ canvas.pack()
 frame = tk.Frame(root, bg='white')
 frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
 
-openFile = tk.Button(root, text='Open File', padx=10, pady=5, fg='white', bg='#263D42')
+openFile = tk.Button(root, text='Open File', padx=10, pady=5, fg='white', bg='#263D42', command=add_app)
 openFile.pack()
 
 runApps = tk.Button(root, text='Run Apps', padx=10, pady=5, fg='white', bg='#263D42')
