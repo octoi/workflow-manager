1174efda57a0d7f136b348cb6c50a98b86169a1e
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-14 15:43:56 +0530

    run app

--- app.py
+++ app.py
@@ -17,6 +17,9 @@ def add_app():
         label = tk.Label(frame, text=app)
         label.pack()
 
+def run_apps():
+    for app in apps:
+        os.startfile(app);
 
 canvas = tk.Canvas(root, height=700, width=700, bg='#263D42')
 canvas.pack()
@@ -27,7 +30,7 @@ frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
 openFile = tk.Button(root, text='Open File', padx=10, pady=5, fg='white', bg='#263D42', command=add_app)
 openFile.pack()
 
-runApps = tk.Button(root, text='Run Apps', padx=10, pady=5, fg='white', bg='#263D42')
+runApps = tk.Button(root, text='Run Apps', padx=10, pady=5, fg='white', bg='#263D42', command=run_apps)
 runApps.pack()
 
 root.mainloop()
