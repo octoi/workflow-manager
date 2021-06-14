7913ce9459c7d7b135545004b18a8ca5e9033923
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-14 15:27:46 +0530

    added open file btn

--- app.py
+++ app.py
@@ -10,4 +10,7 @@ canvas.pack()
 frame = tk.Frame(root, bg='white')
 frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
 
+openFile = tk.Button(root, text='Open File', padx=10, pady=5, fg='white', bg='#263D42')
+openFile.pack()
+
 root.mainloop()
